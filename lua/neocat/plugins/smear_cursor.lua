d3584e3761e8f53df6f86a6192b18d219255d95b
noqmorien <cowretwc@gmail.com>
2025-09-12 22:32:06 +0800

    feat(smear): update cursor smear
    Signed-off-by: noqmorien <cowretwc@gmail.com>

--- lua/neocat/plugins/smear_cursor.lua
+++ lua/neocat/plugins/smear_cursor.lua
@@ -3,13 +3,13 @@ return {
     config = function()
         local smear = require'smear_cursor'
         smear.setup {
-            stifness = 0.8,
-            trailing_stifness = 0.8,
+            stifness = 0.5,
+            trailing_stifness = 0.5,
             stifness_insert_mode = 0.9,
             trailing_stifness_insert_mode = 0.9,
             damping = 0.95,
             damping_insert_mode = 0.95,
-            distance_stop_animating = 0.2,
+            distance_stop_animating = 0.5,
             matrix_pixel_threshold = 0.3,
             time_interval = 7,
         }
