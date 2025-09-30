a38ec1675ebdbc06b6b167ca2d05ce67cddb97cf
noqmorien <cowretwc@gmail.com>
2025-09-30 09:48:39 +0800

    feat: update
    Signed-off-by: noqmorien <cowretwc@gmail.com>

--- lua/neocat/plugins/bufferline.lua
+++ lua/neocat/plugins/bufferline.lua
@@ -8,16 +8,25 @@ return {
                 options = {
                     mode = "buffers",
                     numbers = "ordinal",
-                    offsets = { { filetype = "NERDTree", text = "File Explorer", highlight = "directory", text_align = "left" } },
+                    offsets = {
+                        {
+                            filetype = "NvimTree",
+                            text = function()
+                                return " " .. vim.fn.getcwd()
+                            end,
+                            highlight = "directory",
+                            text_align = "left"
+                        }
+                    },
                     show_tab_indicator = true,
                     diagnostics = "nvim_lsp",
                     diagnostics_indicator = function(count, level)
                         local icon = level:match("error") and "" or level:match("warning") and "" or ""
                         return " (" .. icon .. " " .. count .. ")"
                     end,
-                    separator_style = "slope",
+                    separator_style = "thin",
                     always_show_bufferline = true,
-                }
+                },
             }
         end,
         dependencies = {
