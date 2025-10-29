53d1882dde06565b87054888cb6a0eaa2efb77c6
Mephisto <cowretwc@gmail.com>
2025-10-29 17:52:18 +0800

    chore: last update want to move to arch
    Signed-off-by: Mephisto <cowretwc@gmail.com>

--- lua/neocat/plugins/themes.lua
+++ lua/neocat/plugins/themes.lua
@@ -1,16 +1,16 @@
 return {
-   -- {
-   --     "ellisonleao/gruvbox.nvim",
-   --     priority = 1000,
-   --     config = function()
-   --         require("gruvbox").setup({
-   --             terminal_colors = true,
-   --             dim_inactive = true,
-   --             transparent_mode = true,
-   --         })
-   --         vim.cmd([[colorscheme gruvbox]])
-   --     end,
-   -- }
+  -- {
+  --     "ellisonleao/gruvbox.nvim",
+  --     priority = 1000,
+  --     config = function()
+  --         require("gruvbox").setup({
+  --             terminal_colors = true,
+  --             dim_inactive = true,
+  --             transparent_mode = true,
+  --         })
+  --         vim.cmd([[colorscheme gruvbox]])
+  --     end,
+  -- }
    {
        "catppuccin/nvim",
        name = "catppuccin",
@@ -18,6 +18,11 @@ return {
        config = function()
            require("catppuccin").setup({
                flavour = 'mocha',
+               float = {
+                   transparent_background = true,
+                   solid = true
+               },
+               term_colors = true,
                transparent_background = true,
            })
            vim.cmd([[colorscheme catppuccin]])
